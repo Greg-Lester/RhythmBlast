572891d3afcc73971d15acb6cadd760a1b2a0e7a
Greg Lester <contactgreglester@gmail.com>
2019-02-21 23:31:29 +0100

    wwise audio integration

--- RhythmBlast/Assets/Scripts/CompletePlayerController.cs
+++ RhythmBlast/Assets/Scripts/CompletePlayerController.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class CompletePlayerController : MonoBehaviour
-{
-    private float verticalVelocity;
-    public float speed;
-    private Rigidbody2D rb2d;
-    private int desiredLane = 1; //0 = left, 1= mid, 2 = right
+//public class CompletePlayerController : MonoBehaviour
+//{
+//private float verticalVelocity;
+//public float speed;
+//private Rigidbody2D rb2d;
+//private int desiredLane = 1; //0 = left, 1= mid, 2 = right
 
-    private void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
+//private void Start()
+//    {
+//rb2d = GetComponent<Rigidbody2D>();
+//  }
 
-    private void Update()
-    {
+//private void Update()
+// {
 
-        if (Input.GetKey(KeyCode.D))
-            MoveLane(false);
-        if (Input.GetKey(KeyCode.A))
-            MoveLane(true);
-    }
+//if (Input.GetKey(KeyCode.D))
+//MoveLane(false);
+//if (Input.GetKey(KeyCode.A))
+//MoveLane(true);
+// }
 
-    private void MoveLane(bool goingRight)
-    {
-        //left
-        if (!goingRight)
-    }
+//private void MoveLane(bool goingRight)
+//{
+//left
+//if (!goingRight)
+// }
 
-}
+//}
