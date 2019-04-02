66ff7a87a3c396a64c8e495d0b516d510feff9f3
Greg Lester <contactgreglester@gmail.com>
2019-04-02 05:48:29 +0100

    Revert "revert backup"
    This reverts commit f90737a73651e61c9afd51604603b797603b15c5.

--- RhythmBlast/Assets/Scripts/PlayerMovement.cs
+++ RhythmBlast/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,6 @@
 //using System.Collections.Generic;
 //using UnityEngine;
 
-<<<<<<< HEAD:RhythmBlast/Assets/Scripts/PlayerMovement.cs
 //<<<<<<< HEAD:RhythmBlast/Assets/Scripts/PlayerMovement.cs
 //public class PlayerMovement : MonoBehaviour {
     
@@ -18,14 +17,6 @@
 //private Rigidbody2D rb2d;
 //private int desiredLane = 1; //0 = left, 1= mid, 2 = right
 //>>>>>>> 572891d3afcc73971d15acb6cadd760a1b2a0e7a:RhythmBlast/Assets/Scripts/CompletePlayerController.cs
-=======
-public class CompletePlayerController : MonoBehaviour
-{
-    private float verticalVelocity;
-    public float speed;
-    private Rigidbody2D rb2d;
-    private int desiredLane = 1; //0 = left, 1= mid, 2 = right
->>>>>>> parent of fea0a25... Backup:RhythmBlast/Assets/Scripts/CompletePlayerController.cs
 
 //private void Start()
 //    {
