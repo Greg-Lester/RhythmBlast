84c50266bba1322ac98b92f7491aa31e83d33c4e
Greg Lester <contactgreglester@gmail.com>
2019-04-02 05:41:12 +0100

    remove meta files

--- RhythmBlast/Assets/Scripts/Player.cs
+++ RhythmBlast/Assets/Scripts/Player.cs
@@ -1,24 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    private Vector2 targetPos;
-    public float Xincrement;
-
-    public float speed;
-
-    private void Update()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.A)) {
-            targetPos = new Vector2(transform.position.y, transform.position.x + Xincrement);
-            transform.position = targetPos;
-        } else if (Input.GetKeyDown(KeyCode.A)) {
-            targetPos = new Vector2(transform.position.y, transform.position.x + Xincrement);
-            transform.position = targetPos;
-        }
-}
-}
+//public class Player : MonoBehaviour
+//{
+//    private Vector2 targetPos;
+//
+//    public float speed;
+//
+//    private void Update()
+//    {
+    //        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+//
+//        if (Input.GetKeyDown(KeyCode.A)) {
+//            targetPos = new Vector2(transform.position.y, transform.position.x + Xincrement);
+//            transform.position = targetPos;
+//        } else if (Input.GetKeyDown(KeyCode.A)) {
+    //            targetPos = new Vector2(transform.position.y, transform.position.x + Xincrement);
+//            transform.position = targetPos;
+//        }
+//}
+//}
